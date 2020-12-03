8149fedbe4f57cad7ae783e2a288fef5bee06257
FabianMayer <FabianMayer@0-209-R12>
2020-12-03 12:58:19 +0100

    begin Vertex-Clas

---
+++ zocken2020/src/zocken2020/Test.java
@@ -0,0 +1,18 @@
+package zocken2020;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Vertex v1 = new Vertex(17, 4);
+		Vertex v2 = new Vertex(42, 0);
+		Vertex vv = new Vertex(0, 0);
+
+		System.out.println("v1="+v1.toString());
+		System.out.println("v2="+v2.toString());
+		v1.y = 2 * v1.y;
+
+		System.out.println("v1x2="+v1.toString());
+
+	}
+
+}
---
+++ zocken2020/src/zocken2020/Vertex.java
@@ -0,0 +1,25 @@
+package zocken2020;
+
+public class Vertex {
+
+	public double x;
+	public double y;
+
+	public Vertex(double x, double y) {
+
+		this.x = x;
+		this.y = y;
+	}
+
+	public double length() {
+		return Math.sqrt(x*x+y*y);
+	}
+	
+	public Vertex skalarMult(double s) {
+		return new Vertex(x*s, y*s);
+	}
+	
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
+}
