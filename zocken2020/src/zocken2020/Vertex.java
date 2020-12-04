ab80576d0f9b25d7063999bd1d07568ad0dbcdbc
FabianMayer <FabianMayer@0-208-12>
2020-12-04 10:25:06 +0100

    set Methodes

--- zocken2020/src/zocken2020/Vertex.java
+++ zocken2020/src/zocken2020/Vertex.java
@@ -12,13 +12,35 @@ public class Vertex {
 	}
 
 	public double length() {
-		return Math.sqrt(x*x+y*y);
+		return Math.sqrt(x * x + y * y);
 	}
-	
+
 	public Vertex skalarMult(double s) {
-		return new Vertex(x*s, y*s);
+		return new Vertex(x * s, y * s);
 	}
-	
+
+	public void skalarMultMod(double s) {
+		x = s * x;
+		y = s * y;
+	}
+
+	public Vertex add(Vertex v2) {
+		return new Vertex(x + v2.x, y + v2.y);
+	}
+
+	public void addMod(Vertex v2) {
+		x = x + v2.x;
+		y = y + v2.y;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
 	public String toString() {
 		return "(" + x + "," + y + ")";
 	}
