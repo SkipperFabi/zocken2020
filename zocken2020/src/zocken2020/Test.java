8abe01d52df6d2c0727cbb1aafbe60abd3fe1b7a
FabianMayer <FabianMayer@0-208-12>
2020-12-04 11:04:18 +0100

    test of methodes

--- zocken2020/src/zocken2020/Test.java
+++ zocken2020/src/zocken2020/Test.java
@@ -5,7 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		Vertex v1 = new Vertex(17, 4);
 		Vertex v2 = new Vertex(42, 0);
-		Vertex vv = new Vertex(0, 0);
+		Vertex v3 = new Vertex(0,0);
+		Vertex v4 = new Vertex(42, 23);
+		
+		//Seitenanzahl 54 bzw 67
 
 		System.out.println("v1="+v1.toString());
 		System.out.println("v2="+v2.toString());
